f028c58414537968163500f2ad647c9723c7d297
IdarV <tjoida13@nith.no>
2015-09-17 16:50:19 +0200

    Added support for 'beans-alternative' sources

--- src/main/java/no/westerdals/tjoida/Main.java
+++ src/main/java/no/westerdals/tjoida/Main.java
@@ -17,7 +17,6 @@ public class Main {
     public static void main(String[] args) {
         WeldContainer container = new Weld().initialize();
         Instance<MyService> service = container.instance().select(MyService.class);
-        service.get().printUsers();
-        System.out.println("Hello");
+        service.get().printUserNames();
     }
 }
--- src/main/java/no/westerdals/tjoida/service/InMemoryUserDAO.java
+++ src/main/java/no/westerdals/tjoida/service/InMemoryUserDAO.java
@@ -1,12 +1,14 @@
 package no.westerdals.tjoida.service;
 
+import javax.enterprise.inject.Alternative;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Cyzla on 17.09.2015.
  */
-public class InMemoryUserDAO implements UserDAO {
+@Alternative
+public class InMemoryUserDAO implements UserDAO{
 
     private List<String> users = new ArrayList<>();
 
@@ -18,7 +20,7 @@ public class InMemoryUserDAO implements UserDAO {
     }
 
     @Override
-    public List<String> getUsers() {
+    public List<String> names() {
         System.out.println("In Memory");
         return users;
     }
